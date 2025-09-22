4bd440fc775b5d86d22bf89da7d94e1345a8483f
edgar6021 <115028351+edgar6021@users.noreply.github.com>
2025-09-22 17:14:55 -0400

    update navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-900 text-white p-4 shadow-md">
+    <nav className="bg-red-900 text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">Techeande</h1>
         <ul className="flex gap-4">
